f17872b23207cf484846cb8b796f0f748ec0fce0
K.M. Hasibur Rahman Srijon <116680350+srijon57@users.noreply.github.com>
2022-10-26 19:02:08 +0600

    Create playing with charecters.c

---
+++ playing with charecters.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{   char ch
+    string s,
+    sen scn;
+    scanf("%c %c %c", &ch, &s, &scn);
+    printf("%c\n%c\n%c\n", ch, s, scn);
+   
+    return 0;
+}
